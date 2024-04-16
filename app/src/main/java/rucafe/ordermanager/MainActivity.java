8ec56ce528c5f211722810618c7e9d102885b778
woogyeom <woogyeom99@gmail.com>
2024-04-16 19:04:42 -0400

    Example Button

--- app/src/main/java/rucafe/ordermanager/MainActivity.java
+++ app/src/main/java/rucafe/ordermanager/MainActivity.java
@@ -1,6 +1,10 @@
 package rucafe.ordermanager;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button myButton = findViewById(R.id.myButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupDialog();
+            }
+        });
+    }
+
+    private void showPopupDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Notification");
+        builder.setMessage("Button is clicked.");
+        builder.setPositiveButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
