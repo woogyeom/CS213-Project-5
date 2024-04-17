e0e14ebdb6fc841c42c9ae9d36b85927525aa6ea
Aravind <aravindchundu@gmail.com>
2024-04-17 03:57:13 -0400

    Added buttons for each page (they work)

--- app/src/main/java/rucafe/ordermanager/MainActivity.java
+++ app/src/main/java/rucafe/ordermanager/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -33,8 +36,78 @@ public class MainActivity extends AppCompatActivity {
                 showPopupDialog();
             }
         });
+        */
+
+        // Creating the Fragment Manager
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // For Donuts button
+        Button buttonDonuts = findViewById(R.id.buttonDonuts);
+        buttonDonuts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, DonutFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
+        });
+
+        // For Coffee button
+        Button buttonCoffee = findViewById(R.id.buttonCoffee);
+        buttonCoffee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, CoffeeFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
+        });
+
+        // For Sandwich button
+        Button buttonSandwiches = findViewById(R.id.buttonSandwich);
+        buttonSandwiches.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, SandwichFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
+        });
+
+        // For Current Order button
+        Button buttonCurrentOrder = findViewById(R.id.buttonCurrentOrder);
+        buttonCurrentOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, CurrentOrderFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
+        });
+
+        // For All Order button
+        Button buttonAllOrders = findViewById(R.id.buttonAllOrders);
+        buttonAllOrders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, AllOrdersFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
+        });
     }
 
+    /*
     private void showPopupDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Notification");
@@ -48,4 +121,5 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+    */
 }
